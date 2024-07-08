1181fec7cac60b6a5ad48b2088be5b8e48a36dd0
Patrick Jhonatan Hernandez Blanco <patrickjhonatanh@gmail.com>
2024-07-07 22:24:54 -0600

    Rutas tradicionales sin lazy load
    Creacion de rutas tradicionales, sin lazy load con react router

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,51 @@
+import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router-dom";
+
+import reactLogo from '../assets/react.svg'
+import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+
+export const Navigation = () => {
+
+  return (
+    <BrowserRouter>
+      <div className="main-layout">
+        <nav>
+          <img src={ reactLogo } alt="React loog" />
+          <ul>
+            <li>
+              <NavLink 
+                to="/lazy1" 
+                className={ ({ isActive }) => isActive ? 'nav-active' : '' }
+              >
+                Lazy1
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to="/lazy2" 
+                className={({ isActive }) => isActive ? 'nav-active' : ''}
+              >
+                Lazy2
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to="/lazy3" 
+                className={({ isActive }) => isActive ? 'nav-active' : ''}
+              >
+                Lazy3
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+            <Route path="lazy1" element={ <LazyPage1 /> } />
+            <Route path="lazy2" element={ <LazyPage2 /> } />
+            <Route path="lazy3" element={ <LazyPage3 /> } />
+
+            <Route path="/*"    element={<Navigate to="/lazy1" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
