f02143a7a4a6d1564ecedf0e5e8c2c57ad61a924
Patrick Jhonatan Hernandez Blanco <patrickjhonatanh@gmail.com>
2024-07-17 09:52:48 -0600

    Control props sin controlar el estado

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { onChangeArgs, Product } from "../interfaces/interfaces";
 
 interface useProductsArgs {
@@ -10,15 +10,8 @@ interface useProductsArgs {
 export const useProduct = ({ product, onChange, value = 0 }: useProductsArgs) => {
 
   const [counter, setCounter] = useState<number>( value );
-  const isControlled = useRef( !!onChange );
 
   const increaseBy = (value: number) => {
-
-    if ( isControlled.current ) {
-      return onChange({ count: value, product });
-    }
-    
-
     const newValue = Math.max(counter + value, 0);
     setCounter(newValue );
     onChange && onChange({ count: newValue, product  });
